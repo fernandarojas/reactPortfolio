8ff093dc9b2adf1302603b6bcceabdb259499bf3
Fernanda <fernandarojasc@gmail.com>
2018-10-16 00:52:39 -0400

    Updates

---
+++ precache-manifest.1763246245d52162e083b7e5178f2248.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0bfce8dd94b52aef958",
+    "url": "/reactPortfolio/static/js/runtime~main.b0bfce8d.js"
+  },
+  {
+    "revision": "ea61e5af9a6bd673b9dd",
+    "url": "/reactPortfolio/static/js/main.ea61e5af.chunk.js"
+  },
+  {
+    "revision": "47b1a01099b5162c5953",
+    "url": "/reactPortfolio/static/js/1.47b1a010.chunk.js"
+  },
+  {
+    "revision": "784be0b5dd698ee9eb6dbea78de68842",
+    "url": "/reactPortfolio/index.html"
+  }
+];
